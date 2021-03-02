2838eaf50cb62179af438b83f9e68a52e444573e
lukasbp <44759049+lukasbp@users.noreply.github.com>
2021-03-02 01:40:35 -0300

    parte A

---
+++ lucas_belinelli_pereira/firstNodeProject/Numfinder.js
@@ -0,0 +1,19 @@
+class NumFinder {
+    find(arr) {
+      if (arr.length === 0) throw "empty array";
+  
+      for (let el of arr) {
+        if (typeof el !== "number") throw "invalid array";
+      }
+  
+      this.biggest = arr[0];
+      this.smallest = arr[0];
+  
+      for (let num of arr) {
+        if (num < this.smallest) this.smallest = num;
+        if (num > this.biggest) this.biggest = num;
+      }
+    }
+  }
+  
+  module.exports = NumFinder;
